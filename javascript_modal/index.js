947193b92d4ebfd83799ed97e10b9c3adc3abc90
jihoon <rec8730@gmail.com>
2022-02-14 20:57:18 +0900

    2월14일 input,change와 템플릿 추가

--- javascript_modal/index.js
+++ javascript_modal/index.js
@@ -1,15 +1,26 @@
 
 // 반복문 for문
-let lala = $('.tab-button');
-for (let i = 0; i < lala.length; i++) {
-  $('.tab-button').eq(i).click(function(){
-    $('.tab-button').removeClass('active');
-    $('.tab-content').removeClass('show');
-    
-    $('.tab-button').eq(i).addClass('active');
-    $('.tab-content').eq(i).addClass('show');
-  });
-};
+// let lala = $('.tab-button');
+// for (let i = 0; i < lala.length; i++) {
+//   $('.tab-button').eq(i).click(function(){
+//     탭열기(i)
+//   });
+// };
+
+$('.list').data('작명','값');
+$('.list').data('작명');
+
+$('.list').click(function(e){
+  탭열기(e.target.dataset.id)
+});
+
+function 탭열기(숫자) {
+  $('.tab-button').removeClass('active');
+  $('.tab-content').removeClass('show');
+  
+  $('.tab-button').eq(숫자).addClass('active');
+  $('.tab-content').eq(숫자).addClass('show');
+}
 
 
 // 버튼0 (products)을 누르면...AbortController
